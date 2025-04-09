5322eabf614cca77450ab569b582ec90211e32dc
AndreLuiz <andreluizrc27@gmail.com>
2025-04-09 07:44:46 -0300

    Inserção página de Config na navBar da home

--- app_flutter/lib/pages/Home.dart
+++ app_flutter/lib/pages/Home.dart
@@ -2,6 +2,7 @@ import 'package:curved_nav_bar/curved_bar/curved_action_bar.dart';
 import 'package:curved_nav_bar/fab_bar/fab_bottom_app_bar_item.dart';
 import 'package:curved_nav_bar/flutter_curved_bottom_nav_bar.dart';
 import 'package:flutter/material.dart';
+import 'package:app_flutter/pages/Config.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -11,65 +12,47 @@ class Home extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<Home> {
+
   @override
   Widget build(BuildContext context) {
     return CurvedNavBar(
       actionButton: CurvedActionBar(
-          onTab: (value) {
-            /// perform action here
-            print(value);
-          },
-          activeIcon: Container(
-            padding: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-                color: Colors.white,
-                shape: BoxShape.circle
-            ),
-            child: Icon(
-              Icons.camera_alt,
-              size: 50,
-              color: Colors.teal,
-            ),
+        onTab: (value) {
+         print(value);
+        },
+        activeIcon: Container(
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(color: Colors.white, shape: BoxShape.circle),
+          child: Icon(
+            Icons.camera_alt,
+            size: 50,
+            color: Colors.teal,
           ),
-          inActiveIcon: Container(
-            padding: EdgeInsets.all(8),
-            decoration: BoxDecoration(
-                color: Colors.white70,
-                shape: BoxShape.circle
-            ),
-            child: Icon(
-              Icons.camera_alt_outlined,
-              size: 50,
-              color: Colors.teal,
-            ),
+        ),
+        inActiveIcon: Container(
+          padding: EdgeInsets.all(8),
+          decoration: BoxDecoration(color: Colors.white70, shape: BoxShape.circle),
+          child: Icon(
+            Icons.camera_alt_outlined,
+            size: 50,
+            color: Colors.teal,
           ),
-          text: "Câmera"
+        ),
+        text: "Câmera",
       ),
       activeColor: Colors.white,
       navBarBackgroundColor: Colors.teal,
       inActiveColor: Colors.black45,
       appBarItems: [
         FABBottomAppBarItem(
-            activeIcon: Icon(
-              Icons.home,
-              color: Colors.white,
-            ),
-            inActiveIcon: Icon(
-              Icons.home,
-              color: Colors.black26,
-            ),
-            text: 'Início'
+          activeIcon: Icon(Icons.home, color: Colors.white),
+          inActiveIcon: Icon(Icons.home, color: Colors.black26),
+          text: 'Início',
         ),
         FABBottomAppBarItem(
-            activeIcon: Icon(
-              Icons.settings,
-              color: Colors.white,
-            ),
-            inActiveIcon: Icon(
-              Icons.settings,
-              color: Colors.black26,
-            ),
-            text: 'Configurações'
+          activeIcon: Icon(Icons.settings, color: Colors.white),
+          inActiveIcon: Icon(Icons.settings, color: Colors.black26),
+          text: 'Configurações',
         ),
       ],
       bodyItems: [
@@ -102,18 +85,14 @@ class _HomeScreenState extends State<Home> {
                   size: 30,
                 ),
                 onPressed: () {
-                  // Lógica para abrir perfil ou configurações do usuário
                   print('Perfil do usuário');
                 },
               ),
             ],
           ),
         ),
-        // Wallet screen content
-        Container(
-          height: MediaQuery.of(context).size.height,
-          color: Colors.pinkAccent,
-        )
+        // Config screen content
+        ConfigScreen()
       ],
       actionBarView: Container(
         height: MediaQuery.of(context).size.height,
